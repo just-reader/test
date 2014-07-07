d22c52f221fc5cc8ead3f496364dbef308e3ddad
Timur Lyapin <just.r3ader@gmail.com>
2014-07-07 16:34:07 +0500

    BTest added

---
+++ BTest/BTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh, hi again, world");
+            Console.ReadLine();
+        }
+    }
+}
