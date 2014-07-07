c0c2b4393b15b88823786c0d3d07a7f80ba21985
Timur Lyapin <just.r3ader@gmail.com>
2014-07-07 16:35:32 +0500

    frist

---
+++ CTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("And again, hi, world");
+            Console.ReadLine();
+        }
+    }
+}
